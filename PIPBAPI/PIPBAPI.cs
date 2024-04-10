06d9f70cfa954b9ff1cb3a40b30cd9f7e1555875
VinEckSie <159125909+VinEckSie@users.noreply.github.com>
2024-04-10 07:37:36 +0000

    ïnit

--- PIPBAPI/PIPBAPI.cs
+++ PIPBAPI/PIPBAPI.cs
@@ -10,7 +10,6 @@ using Newtonsoft.Json;
 using System.Net.Http.Headers;
 using System.Text;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
 using PocketInvest.LoanResponseModels;
 
 
@@ -21,114 +20,93 @@ namespace PocketInvest.PIPBAPI
          private static HttpClient client = new HttpClient();
         private static string jwtToken;
 
-    [FunctionName("PIPBAPI")]
-    public static async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", "options", Route = null)] HttpRequest req,
-        ILogger log)
-    {
-        log.LogInformation("C# HTTP trigger function processed a request.");
-
-        // Authenticate with the API
-        // var username = Environment.GetEnvironmentVariable("vesiv@proton.me");
-        // var password = Environment.GetEnvironmentVariable("UUeK&3iaFaAr$T-J");
-        var username = "vesiv@proton.me";
-        var passwordVar = "UUeK&3iaFaAr$T-J";
-        await Authenticate(username, passwordVar);
-
-        // Get the list of loans
-        var loans = await GetLoans();
-
-        return new OkObjectResult(loans);
-       //return new OkObjectResult("Hello World");
-    }
-
-    private static async Task Authenticate(string username, string _password)
-    {
-        var loginUrl = "https://api.peerberry.com/v1/investor/login";
-        var payload = new { email = username, password = _password };
-        var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
-
-        // Print the request payload
-        Console.WriteLine($"Request payload: {JsonConvert.SerializeObject(payload)}");
-
-        var response = await client.PostAsync(loginUrl, content);   
-
-        if (response.IsSuccessStatusCode)
+        [FunctionName("PIPBAPI")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+            ILogger log)
         {
-            var responseBody = await response.Content.ReadAsStringAsync();
-            dynamic data = JsonConvert.DeserializeObject(responseBody);
-            jwtToken = data.access_token;
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            // Authenticate with the API
+            var username = Environment.GetEnvironmentVariable("PBUSN");
+            var password = Environment.GetEnvironmentVariable("PBPWD");
+
+            await Authenticate(username, password);
+
+            // Get the list of loans
+            var loans = await GetLoans();
+
+            return new OkObjectResult(loans);
         }
-        else
+
+        private static async Task Authenticate(string username, string _password)
         {
-            // Print the response status and content
-            Console.WriteLine($"Response status: {response.StatusCode}");
-            Console.WriteLine($"Response content: {await response.Content.ReadAsStringAsync()}");
+            var loginUrl = "https://api.peerberry.com/v1/investor/login";
+            var payload = new { email = username, password = _password };
+            var content = new StringContent(JsonConvert.SerializeObject(payload), Encoding.UTF8, "application/json");
 
-            var contentString = await content.ReadAsStringAsync();
-            throw new Exception("Authentication failed. Content: " + contentString);
-        }
-    }
+            // Print the request payload
+            Console.WriteLine($"Request payload: {JsonConvert.SerializeObject(payload)}");
 
-    private static async Task<dynamic> GetLoans()
-    {
-        Console.WriteLine("Get loans start process");
-        var loansUrl = "https://api.peerberry.com/v1/loans";
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
-        var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, loansUrl));
+            var response = await client.PostAsync(loginUrl, content);   
 
-        if (response.IsSuccessStatusCode)
-        {
-            Console.WriteLine("Get loans process success");
-            var responseBody = await response.Content.ReadAsStringAsync();
-            LoanResponse loans = JsonConvert.DeserializeObject<LoanResponse>(responseBody);
-
-            foreach (var loan in loans.Data)
+            if (response.IsSuccessStatusCode)
             {
-                Console.WriteLine($@"LoanId: {loan.LoanId}
-                Country: {loan.Country}
-                InterestRate: {loan.InterestRate}
-                LoanOriginator: {loan.loanOriginator}
-                OriginatorId: {loan.originatorId}
-                IssuedDate: {loan.issuedDate}
-                FinalPaymentDate: {loan.finalPaymentDate}
-                TermType: {loan.termType}
-                Status: {loan.status}
-                RemainingTerm: {loan.remainingTerm}
-                InitialTerm: {loan.initialTerm}
-                LoanAmount: {loan.loanAmount}
-                AvailableToInvest: {loan.availableToInvest}
-                MinimumInvestmentAmount: {loan.minimumInvestmentAmount}
-                InvestedAmount: {loan.investedAmount}
-                Currency: {loan.currency}
-                Buyback: {loan.buyback}");
+                var responseBody = await response.Content.ReadAsStringAsync();
+                dynamic data = JsonConvert.DeserializeObject(responseBody);
+                jwtToken = data.access_token;
             }
-            return loans;
+            else
+            {
+                // Print the response status and content
+                Console.WriteLine($"Response status: {response.StatusCode}");
+                Console.WriteLine($"Response content: {await response.Content.ReadAsStringAsync()}");
+
+                var contentString = await content.ReadAsStringAsync();
+                throw new Exception("Authentication failed. Content: " + contentString);
+            }
         }
-        else
+
+        private static async Task<dynamic> GetLoans()
         {
-            Console.WriteLine("Get loans process failed");
-            throw new Exception("Failed to get loans");
+            Console.WriteLine("Get loans start process");
+            var loansUrl = "https://api.peerberry.com/v1/loans";
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
+            var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, loansUrl));
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("Get loans process success");
+                var responseBody = await response.Content.ReadAsStringAsync();
+                LoanResponse loans = JsonConvert.DeserializeObject<LoanResponse>(responseBody);
+
+                foreach (var loan in loans.Data)
+                {
+                    Console.WriteLine($@"LoanId: {loan.LoanId}
+                    Country: {loan.Country}
+                    InterestRate: {loan.InterestRate}
+                    LoanOriginator: {loan.loanOriginator}
+                    OriginatorId: {loan.originatorId}
+                    IssuedDate: {loan.issuedDate}
+                    FinalPaymentDate: {loan.finalPaymentDate}
+                    TermType: {loan.termType}
+                    Status: {loan.status}
+                    RemainingTerm: {loan.remainingTerm}
+                    InitialTerm: {loan.initialTerm}
+                    LoanAmount: {loan.loanAmount}
+                    AvailableToInvest: {loan.availableToInvest}
+                    MinimumInvestmentAmount: {loan.minimumInvestmentAmount}
+                    InvestedAmount: {loan.investedAmount}
+                    Currency: {loan.currency}
+                    Buyback: {loan.buyback}");
+                }
+                return loans;
+            }
+            else
+            {
+                Console.WriteLine("Get loans process failed");
+                throw new Exception("Failed to get loans");
+            }
         }
-    }
-        // [FunctionName("PIPBAPI")]
-        // public static async Task<IActionResult> Run(
-        //     [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-        //     ILogger log)
-        // {
-        //     log.LogInformation("C# HTTP trigger function processed a request.");
-
-        //     string name = req.Query["name"];
-
-        //     string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-        //     dynamic data = JsonConvert.DeserializeObject(requestBody);
-        //     name = name ?? data?.name;
-
-        //     string responseMessage = string.IsNullOrEmpty(name)
-        //         ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-        //         : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-        //     return new OkObjectResult(responseMessage);
-        // }
     }
 }
